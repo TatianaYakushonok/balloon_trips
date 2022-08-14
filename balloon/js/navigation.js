81b64243172500e5ecee7598f7b2a5806aa3a7ae
TatianaYakushonok <yakush2@yandex.ru>
2022-08-15 00:55:12 +0700

    feat: add gulp in project (Mon, 15 Aug 2022 00:55:11 +0700)

--- balloon/js/navigation.js
+++
@@ -1,6 +0,0 @@
-const navigationLink = document.querySelectorAll('.navigation__link');
-const navToggle = document.querySelector('#nav-toggle');
-
-navigationLink.forEach(link => {
-    link.addEventListener('click', () => navToggle.checked = false )
-})
